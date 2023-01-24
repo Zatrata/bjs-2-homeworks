c567d757e6e3f118f96e03818faae6c6046fc261
M.Vedishchev <vedishchev.ms@gmail.com>
2023-01-25 00:13:41 +0400

    decision tasks 1-3

--- 2.functions/task.js
+++ 2.functions/task.js
@@ -1,8 +1,40 @@
 function getArrayParams(...arr) {
+  let min = -Infinity;
+  let max = Infinity;
+  let sum = 0;
+  for (let i = 0; i < arr.length; i++){
+    sum += arr[i]
+  }
+  avg = (sum / arr.length).toFixed(2);
 
-  return { min: min, max: max, avg: avg };
+  return { min:  Math.min(...arr), max: Math.max(...arr), avg: avg };
 }
 
+let arrOfArr=[[1, 2, 3, 4], [10, 20, -10, -20]];
+function worker(arr) {
+   for (let i = 0; i < arr.length; i++) {
+    sum += arr[i];
+  }
+  return sum;
+}
+ function makeWork(arrOfArr, worker) {
+  let max = -Infinity;
+  for (let i = 0; i < arrOfArr.length; i++) {
+    let totalSum = worker (arrOfArr[i]);
+    if (totalSum > max){
+      max = totalSum;
+    }
+  }
+  return max;
+  function worker2(arr) {
+    let min = Math.min(arr[i]);
+    let max = Math.max(arr[i]);
+    let difference = Math.abs(min-max);
+   }
+   return makeWork();
+ }
+
+/*
 function summElementsWorker(...arr) {
 
 }
@@ -22,3 +54,4 @@ function averageEvenElementsWorker(...arr) {
 function makeWork (arrOfArr, func) {
 
 }
+*/
