1266b656389ba2802f337ade913735dea778e08e
M.Vedishchev <vedishchev.ms@gmail.com>
2023-01-26 20:24:57 +0400

    corrected remarks

--- 2.functions/task.js
+++ 2.functions/task.js
@@ -1,57 +1,43 @@
 function getArrayParams(...arr) {
-  let min = -Infinity;
-  let max = Infinity;
+  let avg = 0;
   let sum = 0;
   for (let i = 0; i < arr.length; i++){
     sum += arr[i]
   }
   avg = (sum / arr.length).toFixed(2);
-
   return { min:  Math.min(...arr), max: Math.max(...arr), avg: avg };
 }
 
-let arrOfArr=[[1, 2, 3, 4], [10, 20, -10, -20]];
 function worker(arr) {
+  let sum = 0;
    for (let i = 0; i < arr.length; i++) {
     sum += arr[i];
   }
   return sum;
 }
- function makeWork(arrOfArr, worker) {
+ function makeWork(arrOfArr, func) {
   let max = -Infinity;
   for (let i = 0; i < arrOfArr.length; i++) {
-    let totalSum = worker (arrOfArr[i]);
+    let totalSum = func (arrOfArr[i]);
     if (totalSum > max){
       max = totalSum;
     }
   }
   return max;
-  function worker2(arr) {
-    let min = Math.min(arr[i]);
-    let max = Math.max(arr[i]);
-    let difference = Math.abs(min-max);
-   }
-   return makeWork();
  }
 
-/*
-function summElementsWorker(...arr) {
-
-}
-
-function differenceMaxMinWorker(...arr) {
-
-}
-
-function differenceEvenOddWorker(...arr) {
-
-}
-
-function averageEvenElementsWorker(...arr) {
-
-}
-
-function makeWork (arrOfArr, func) {
-
-}
-*/
+ function worker2(arr) {
+  let min = Infinity;
+  let max = -Infinity;
+  let difference = 0;
+  for (let i = 0; i < arr.length; i++){
+    if (arr[i] < min) {
+      min = arr[i];
+    }
+    if (arr[i] > max) {
+      max = arr[i];
+    }
+  }
+  difference = Math.abs (min-max);
+  return difference;
+ }
