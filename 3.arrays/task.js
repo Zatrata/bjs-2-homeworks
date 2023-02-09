2c9fa64152552400e7be0872164771357a2e84e2
M.Vedishchev <vedishchev.ms@gmail.com>
2023-02-09 18:10:02 +0400

    solution of tasks 1 and 2

--- 3.arrays/task.js
+++ 3.arrays/task.js
@@ -1,7 +1,8 @@
 function compareArrays(arr1, arr2) {
-  
+    return arr1.length === arr2.length && arr1.every((element, i) => element === arr2[i]);
 }
 
-function getUsersNamesInAgeRange(users, gender) {
-  
+function advancedFilter(arr) {
+    let result = arr.filter(number => number > 0).filter(number => number % 3 === 0).map(number => number * 10);
+    return result;
 }
