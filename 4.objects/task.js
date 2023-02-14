5fe0361d2e0cf9d546a25a8f6da5519ac372e017
M.Vedishchev <vedishchev.ms@gmail.com>
2023-02-14 22:30:20 +0400

    solution of tasks 1

--- 4.objects/task.js
+++ 4.objects/task.js
@@ -1,19 +1,46 @@
 function Student(name, gender, age) {
-  
+  this.name = name;
+  this.gender = gender;
+  this. age = age;
 }
 
 Student.prototype.setSubject = function (subjectName) {
-  
+  this.subject = subjectName;
 }
 
-Student.prototype.addMarks = function (...marks) {
-  
+Student.prototype.addMark = function (mark) {
+  if (this.marks === undefined) {
+    this.marks = [mark];
+  }
+  else {
+    this.marks.push(mark);
+  }
 }
 
-Student.prototype.getAverage = function () {
-  
+Student.prototype.addMarks = function (...mark) {
+    if (this.marks === undefined) {
+        this.marks = [...mark];
+      }
+      else {
+        this.marks.push(...mark);
+      }
+}
+
+Student.prototype.getAverage = function (marks) {
+  let sumMarks = 0;
+  if (this.marks === undefined) {
+    return ("Оценок нет");
+  }
+  else {
+    for (let i = 0; i < this.marks.length; i++){
+        sumMarks += this.marks[i];
+    }
+    return sumMarks/this.marks.length;
+  }
 }
 
 Student.prototype.exclude = function (reason) {
-  
+  this.excluded = reason;
+  delete this.subject;
+  delete this.marks
 }
